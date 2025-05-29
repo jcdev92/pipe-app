3c4facfb9d40f50a602766b1a3d54a2c00db9158
jcdev92 <jcdeveloper92@gmail.com>
2025-05-29 16:09:04 -0400

    feat(new pipe): :sparkles: add date time pipes

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,13 +1,25 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
   nameUpper = signal('GORDO')
   nameLower = signal('gordo')
   fullName = signal('GoRdO cLaRkE')
+
+  customDate = signal(new Date())
+
+  tickingDateEffect = effect((onCleanup) => {
+    
+    const interval = setInterval(() => {
+        this.customDate.set(new Date());
+      }, 1000);
+
+      onCleanup(() => clearInterval(interval))
+    }
+  );
 }
