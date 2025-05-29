fac10a58779bd84d31593668bedddaf4a0d15e18
jcdev92 <jcdeveloper92@gmail.com>
2025-05-29 16:07:36 -0400

    feat(config): :sparkles: add local configuration time zone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import localEs from '@angular/common/locales/es-VE';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: 'LOCAL_ID',
+      useValue: 'es'
+    }
+  ]
 };
